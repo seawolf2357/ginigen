3d626b26db6db413fb72fc631838d31ac34fae2a
seawolf <125524687+seawolf2357@users.noreply.github.com>
2025-01-13 17:02:34 +0900

    Update empty-screen.tsx

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -3,21 +3,13 @@ import { ArrowRight } from 'lucide-react'
 
 const exampleMessages = [
   {
-    heading: '오늘의 주요 뉴스 헤드라인이 궁금해요',
-    message: '오늘의 주요 뉴스 헤드라인이 궁금해요'
+    heading: '오늘 한국의 주요 뉴스 헤드라인이 궁금해요',
+    message: '오늘 한국 주요 뉴스 헤드라인이 궁금해요'
   },
   {
     heading: '삼성전자 최신 실적 분석해줘',
     message: '삼성전자 최신 실적 분석해줘'
   },
-  {
-    heading: '현대차 vs 테슬라 비교',
-    message: '현대차 vs 테슬라 비교'
-  },
-  {
-    heading: '한국의 인공지능 산업 현황 설명해줘',
-    message: '한국의 인공지능 산업 현황 설명해줘'
-  }
 ]
 
 export function EmptyScreen({
